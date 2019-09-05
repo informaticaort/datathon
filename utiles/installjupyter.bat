ec91867c42d687c1e80a9fd5365486b735fab255
EZEQUIEL.BINKER <ebinker@ort.edu.ar>
2019-09-05 12:06:58 -0300

    Agrego archivos utiles

---
+++ utiles/installjupyter.bat
@@ -0,0 +1,13 @@
+C:
+pip install jupyter
+python -m pip install --upgrade pip
+Z:
+pip install pandas
+pip install matplotlib
+pip install spotipy
+pip install bs4
+pip install image
+pip install stop_words
+pip install wordcloud
+
+jupyter notebook
